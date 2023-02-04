581b6832275364837874dcd09c133319f12e4d1f
shengu <3401514356@qq.com>
2023-02-04 08:28:03 +0800

    第一次提交

---
+++ src/main/java/com/sky/test.java
@@ -0,0 +1,14 @@
+package com.sky;
+
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Author 深谷
+ * @Date 2023/2/4 8:25
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("初次使用git");
+    }
+}
